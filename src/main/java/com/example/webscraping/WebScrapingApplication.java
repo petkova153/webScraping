9a2c2b58feae464f992b00c8d568aa4ee292d7f3
petko <petkova153@gmail.com>
2023-10-02 23:06:05 +0300

    Generated links to products, category definition, number of pages detected and scrolling idea almost figured out

--- src/main/java/com/example/webscraping/WebScrapingApplication.java
+++ src/main/java/com/example/webscraping/WebScrapingApplication.java
@@ -19,7 +19,7 @@ public class WebScrapingApplication {
 
 		//SpringApplication.run(WebScrapingApplication.class, args);
 		try{
-		String url = "https://www.rimi.lt/e-parduotuve/lt/produktai/vaisiai-darzoves-ir-geles/vaisiai-ir-uogos/c/SH-15-3?page=1&pageSize=80&query=%3Arelevance%3AallCategories%3ASH-15-3%3AassortmentStatus%3AinAssortment";
+		String url = "https://www.barbora.lt/darzoves-ir-vaisiai/darzoves-ir-grybai";
 		URL obj = new URL(url);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 // optional request header
@@ -35,23 +35,39 @@ public class WebScrapingApplication {
 		String html = response.toString();
 			Document doc = Jsoup.parse(html);
 
-//			//barbora
-//			Elements products  = doc.select("div.b-product--wrap2");
-//
-//			for (Element product : products){
-//
-//				Elements spans = product.select("img");
-//				for (Element link : spans) {
-//					String href = link.attr("alt");
-//					System.out.println(href);
-//				}
-//
-//				Elements prices = product.select("span.b-product-price-current-number");
-//				for (Element price : prices) {
-//					String prc = price.attr("content");
-//					System.out.println(prc);
-//				}
-//			}
+			//barbora
+			Elements products  = doc.select("div.b-product--wrap2");
+			String shop = url.substring(url.indexOf("www."),url.indexOf(".lt")+3);
+			Elements categories = doc.select("ul.pagination");
+			for (Element category: categories){
+				Elements pages = category.select("a");
+				for (Element page: pages){
+					String links = page.attr("href");
+					System.out.println(links);
+				}
+				break;
+			}
+
+			for (Element product : products){
+				System.out.println("Shop: " + shop);
+				Elements spans = product.select("img");
+				for (Element link : spans) {
+					String productName = link.attr("alt");
+					System.out.println("Product: " + productName);
+				}
+
+				Elements prices = product.select("span.b-product-price-current-number");
+				for (Element price : prices) {
+					String prc = price.attr("content");
+					System.out.println("Price: " + prc);
+				}
+				Elements hrefs = product.select("a.b-product--imagelink");
+				for (Element href : hrefs) {
+					String uRLS = href.attr("href");
+					System.out.println("URL: " + shop+uRLS);
+				}
+				System.out.println("Category: " + url.substring(url.indexOf(".lt/")+3));
+			}
 
 //iki
 //			Elements products  = doc.select("div.css-jpvoog");
@@ -68,30 +84,36 @@ public class WebScrapingApplication {
 //					System.out.println(price);
 //				}
 //			}
-
-//rimi
-			Elements products  = doc.select("li.product-grid__item");
-			for (Element product : products){
-
-				Elements spans = product.select("p.card__name");
-				for (Element link : spans) {
-					String pName = link.toString();
-					String picture = pName.substring(pName.indexOf("e\">")+3,pName.indexOf("</"));
-					System.out.println(picture);
-				}
-
-				Elements prices = product.select("p.card__price-per");
-				for (Element price : prices) {
-					String pPrice = price.toString();
-					String cleanPrice = pPrice.substring(pPrice.indexOf("r\">")+3,pPrice.indexOf("</"));
-					System.out.println(cleanPrice);
-				}
-			}
-
+////rimi
+//			String shop = url.substring(url.indexOf("www."),url.indexOf(".lt")+3);
+//			Elements products  = doc.select("li.product-grid__item");
+//			for (Element product : products){
+//				System.out.println("Shop: " + shop);
+//				Elements spans = product.select("p.card__name");
+//				for (Element link : spans) {
+//					String pName = link.toString();
+//					String productP = pName.substring(pName.indexOf("e\">")+3,pName.indexOf("</"));
+//					System.out.println("Product: " + productP);
+//				}
+//				Elements prices = product.select("p.card__price-per");
+//				for (Element price : prices) {
+//					String pPrice = price.toString();
+//					String cleanPrice = pPrice.substring(pPrice.indexOf("r\">")+3,pPrice.indexOf("</"));
+//					System.out.println("Price: " + cleanPrice);
+//				}
+//				Elements hrefs = product.select("a.card__url");
+//				for (Element href : hrefs) {
+//					String uRLS = href.attr("href");
+//					System.out.println("URL: " + shop+uRLS);
+//				}
+//				Elements categories = doc.select("main");
+//				for (Element category: categories){
+//					System.out.println("Category: " + category.attr("data-gtms-content-category"));
+//				}
+//			}
 		}
 		catch(Exception e){
 			e.getMessage();
 		}
 	}
-
 }
